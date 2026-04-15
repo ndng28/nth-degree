3261e5f6671a3d509069443b91a8266fb1f62031
Chanakya <chanakya@ddght.net>
2026-04-15 04:28:32 +0000

    fix: remove cloudflare adapter main entrypoint from wrangler

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: 'https://blog.ddght.net',
-  adapter: cloudflare(),
 });
